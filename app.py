4b27586d3c3c68a7ceaa4ce84da98c0f0564fa35
VioletteF <violette-f@hotmail.f>
2025-06-12 09:34:14 +0200

    ajout gitgignore (non prise en compte des csv), ajout fichier machine learning, ajout debut application

--- app.py
+++ app.py
@@ -24,16 +24,70 @@ import pandas as pd
 #     )
 # )
 
-df = pd.read_csv("tmdb_full.csv")
+df = pd.read_csv("final_poster.csv")
 
 titres_films = df['title']
 titres_films =(titres_films).sort_values()
 
+debut_lien='https://image.tmdb.org/t/p/w500/'
+
 st.title("Bienvenue dans notre moteur de recherche!")
 
 film_depart=st.selectbox("Selectionne un film que tu as aimé", titres_films)
-checkbox_film=st.checkbox("Informations sur le film")
-checkbox_recommand=st.checkbox("Films similaires")
+col1, col2 = st.columns(2)
+with col1:
+    checkbox_film=st.checkbox("Informations sur le film")
+with col2:
+    checkbox_recommand=st.checkbox("Films similaires")
+
+if checkbox_film:
+    fin_lien=df[df['title'] == film_depart]['poster_path'].values[0]
+    lien=debut_lien+fin_lien
+    st.image(lien)
+
+
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("{lien}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+        }}
+
+        .stApp::before {{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0.5);  /* Ajuste ici pour plus ou moins de transparence */
+            z-index: -1;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    
+    
+    st.markdown(f"""
+        <div style="background-color: rgba(255, 255, 255, 0.7)
+                    ; padding: 1rem; border-radius: 10px;">
+    
+        **Résumé du film** : {df[df['title'] == film_depart]['overview'].values[0]}  
+        **Titre** : {film_depart}  
+        **Année** : {df[df['title'] == film_depart]['release_date'].values[0]}  
+        **Genres** : {df[df['title'] == film_depart]['genres'].values[0]}  
+        **Note** : {df[df['title'] == film_depart]['averageRating'].values[0]}  
+
+        </div>
+        """, unsafe_allow_html=True)
+
 
 
 
